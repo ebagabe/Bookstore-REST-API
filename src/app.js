2eb99623895dda267ff1f935ffd13a7222ee47ec
ebagabe <gabriel.ebagabe@gmail.com>
2024-01-10 15:20:18 -0300

    Creating the route books

--- src/app.js
+++ src/app.js
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
+const books = [
+    {
+        id: 1,
+        title: 'The Lord of the Rings'
+    },
+    {
+        id: 2,
+        title: 'The Hobbit'
+    }
+];
+
 app.get("/", (req, res) => {
     res.status(200).send("NodeJs and MongoDB & Express")
 });
+
+app.get('/books', (req, res) => {
+    res.status(200).json(books)
+})
 
 export default app;
