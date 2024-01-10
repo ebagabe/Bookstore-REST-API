f39e24cb86c49160ddb33b6537844e3df0943a3e
ebagabe <gabriel.ebagabe@gmail.com>
2024-01-10 04:05:05 -0300

    Creating routes

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const routes = {
+    "/": "Using the HTTP and Express library for request",
+    "/books": "I Joined the book route",
+    "/authors": "List of authors"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Using the HTTP library for request");
+    res.end(routes[req.url]);
 });
 
 server.listen(PORT, () => {
