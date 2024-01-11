73803ec338c44dc0942b06356cb3383afc08d887
ebagabe <gabriel.ebagabe@gmail.com>
2024-01-11 14:52:32 -0300

    Creating the schema for the book model

---
+++ src/models/Book.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId },
+    title: { type: String, required: true },
+    publisher: { type: String },
+    price: { type: Number },
+    pages: { type: Number }
+}, { versionKey: false })
+
+const book = mongoose.model("books", bookSchema);
+
+export default book;
