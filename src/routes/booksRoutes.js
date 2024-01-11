cc2fe15a96c8b728b45eb6be336064415f045a79
ebagabe <gabriel.ebagabe@gmail.com>
2024-01-11 18:35:05 -0300

    update the book route

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -7,5 +7,6 @@ routes.get("/books", BookController.ListBooks);
 routes.get("/books/:id", BookController.BookById);
 routes.post("/books", BookController.RegisterBook);
 routes.put("/books/:id", BookController.updateBook);
+routes.delete("/books/:id", BookController.deleteBook);
 
 export default routes;
